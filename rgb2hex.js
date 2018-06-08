0d69662961aecd59039c840bcb1ae3d3a68d8121
usmonster <akeju00+github@gmail.com>
2018-06-08 16:13:31 +0200

    fixes regex to avoid catastrophic backtracking
    Fixes christian-bromann/rgb2hex#1

--- rgb2hex.js
+++ rgb2hex.js
@@ -14,17 +14,17 @@
         }
 
         // parse input
-        var digits = /(.*?)rgb(a)*\((\d+),(\d+),(\d+)(,[0-9]*\.*[0-9]+)*\)/.exec(color.replace(/\s+/g,''));
+        var digits = /^rgba?\((\d+),(\d+),(\d+)(,[0-1]?\.?[0-9]+)?\)$/.exec(color.replace(/\s+/g,''));
 
         if(!digits) {
             // or throw error if input isn't a valid rgb(a) color
             throw new Error('given color (' + color + ') isn\'t a valid rgb or rgba color');
         }
 
-        var red = parseInt(digits[3]);
-        var green = parseInt(digits[4]);
-        var blue = parseInt(digits[5]);
-        var alpha = digits[6] ? /([0-9\.]+)/.exec(digits[6])[0] : '1';
+        var red = parseInt(digits[1]);
+        var green = parseInt(digits[2]);
+        var blue = parseInt(digits[3]);
+        var alpha = digits[4] ? /([0-9\.]+)/.exec(digits[4])[0] : '1';
         var rgb = ((blue | green << 8 | red << 16) | 1 << 24).toString(16).slice(1);
 
         // parse alpha value into float
@@ -41,9 +41,9 @@
         alpha = parseFloat(Math.round(alpha * 100), 10) / 100;
 
         return {
-            hex: digits[1] + '#' + rgb.toString(16),
+            hex: '#' + rgb.toString(16),
             alpha: alpha
-        }
+        };
 
     };
 
